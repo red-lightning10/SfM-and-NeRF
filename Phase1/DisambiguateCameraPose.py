4cbd1023ba5cdc976d73e9727433c0f3f4547e0c
red-lightning10 <inkredible2599@gmail.com>
2024-02-21 00:15:57 -0500

    Typo fixed

--- Phase1/DisambiguateCameraPose.py
+++ Phase1/DisambiguateCameraPose.py
@@ -5,13 +5,13 @@ def DisambiguateCameraPose(Cset, Rset, Xset):
     max_points_in_front = 0
     max_X_in_front = []
     for i in range(len(Cset)):
-        r3 = Rset[i][:, 2]
+        r3 = Rset[i][2]
         C = Cset[i]
         X = Xset[i]
         # print(X)
         num_points_in_front = []
         # num_points_in_front = [np.dot(r3,(X[j] - C)) > 0  for j in range(len(X))]
-        num_points_in_front = [r3 @ (X[j] - C).T > 0 for j in range(len(X)) if X[j][2] > 0]
+        num_points_in_front = [r3.T @ (X[j] - C).T > 0 for j in range(len(X))]
         if sum(num_points_in_front) > max_points_in_front:
             max_points_in_front = sum(num_points_in_front)
             C = Cset[i]
