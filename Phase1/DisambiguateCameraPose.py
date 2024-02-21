e3cad97baad3de2719d295d2f6983c5b12e5c6e3
red-lightning10 <inkredible2599@gmail.com>
2024-02-20 22:35:01 -0500

    Minor Bug Fixes

--- Phase1/DisambiguateCameraPose.py
+++ Phase1/DisambiguateCameraPose.py
@@ -1,15 +1,21 @@
+import numpy as np
+
 def DisambiguateCameraPose(Cset, Rset, Xset):
 
     max_points_in_front = 0
+    max_X_in_front = []
     for i in range(len(Cset)):
-        r3 = Rset[i][2]
+        r3 = Rset[i][:, 2]
         C = Cset[i]
-
-        num_points_in_front = [r3*(Xset[i] - C) > 0 for i in range(len(Xset))]
-
+        X = Xset[i]
+        # print(X)
+        num_points_in_front = []
+        # num_points_in_front = [np.dot(r3,(X[j] - C)) > 0  for j in range(len(X))]
+        num_points_in_front = [r3 @ (X[j] - C).T > 0 for j in range(len(X)) if X[j][2] > 0]
         if sum(num_points_in_front) > max_points_in_front:
             max_points_in_front = sum(num_points_in_front)
             C = Cset[i]
             R = Rset[i]
-
-    return C, R
+            max_X_in_front = Xset[i]
+    print("max", np.shape(max_X_in_front), Xset[0].shape)
+    return C.reshape(3,1), R, max_X_in_front
--- Phase1/LinearTriangulation.py
+++ Phase1/LinearTriangulation.py
@@ -4,21 +4,45 @@ def skew(x):
      return np.array([[ 0  , -x[2],  x[1]],
                      [x[2],    0 , -x[0]],
                      [-x[1], x[0],    0]])
-def triangulation(K,C,R,v1,v2):
-    c1 = C[0]
-    c2 = C[1]
-    r1 = R[0]
-    r2 = R[1]
-    T1 = r1*c1
-    T2 = r2*c2
-    P1 = np.dot(K,np.dot(r1,np.hstack(np.identity(3),-c1)))
-    P2 = np.dot(K,np.dot(r2,np.hstack(np.identity(3),-c2)))
+def homogenize(x):
+    """
+    N x m -> N x (m+1)
+    """
+    X = np.concatenate((x, np.ones((x.shape[0],1))),axis=1)
+    return X
 
+def create_projection_matrix(K, C, R):
+    T = R @ C
+    P = K @ np.hstack((R, T))
+    return P
+
+def LinearTriangulation(K, C_ref, R_ref, C, R, V):
+    
+    c1 = np.reshape(C_ref,(3,1))
+    c2 = np.reshape(C,(3,1))
+    r1 = np.array(R_ref)
+    r2 = np.array(R)
+    v1 = V[:, 0, :]
+    v2 = V[:, 1, :]
+    v1 = homogenize(v1)
+    v2 = homogenize(v2)
+    P1 = create_projection_matrix(K, c1, r1)
+    P2 = create_projection_matrix(K, c2, r2)
+
+    X = []
     for p1,p2 in zip(v1,v2):
-        a1 = skew(p1)*P1
-        a2 = skew(p2)*P2
-        A = np.vstack(a1,a2)
+        a1 = skew(p1) @ P1 
+        a2 = skew(p2) @ P2
+        A = np.vstack((a1,a2))
 
-        U,D,V = np.linalg.svd(A)
-        
+        _, D, VT = np.linalg.svd(A)
+        x = VT[np.argmin(D), :]
+        x = x/x[3]
+        x = x[0:3]
+        x = np.array(x)
+        X.append(x)
+    X = np.vstack(X)
+    print(X)
+    # print(X)
 
+    return X
