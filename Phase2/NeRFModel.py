d86838aac8f87f67ca5d5cc264ac562cb7b91ea1
Adhithiyan03 <83748642+Adhithiyan03@users.noreply.github.com>
2024-03-08 12:34:25 -0500

    phase 2

--- Phase2/NeRFModel.py
+++ Phase2/NeRFModel.py
@@ -9,7 +9,25 @@ class NeRFmodel(nn.Module):
         #############################
         # network initialization
         #############################
-
+        self.layer1 = nn.Sequential(
+            nn.Linear(3 + 3*2*6, 256),
+            nn.Relu(),
+        )
+        self.layer2 = nn.Sequential(
+            nn.Linear(256,256),
+            nn.Relu(),
+        )
+        self.layer3 = nn.Sequential(
+            nn.Linear(256+39, 256),
+            nn.Relu(),
+        )
+        self.layer4 = nn.Sequential(
+            nn.Linear(256,128),
+            nn.Relu(),
+        )
+        self.layer5 = nn.Sequential(
+            nn.Linear(128,4),
+        )
     def position_encoding(self, x, L):
         #############################
         # Implement position encoding here
@@ -21,5 +39,13 @@ class NeRFmodel(nn.Module):
         #############################
         # network structure
         #############################
-
+        x = self.l1(gamma)  #39-256
+        x = self.l2(x)      #256-256
+        x = self.l2(x)      #256-256
+        x = self.l2(x)      #256-256
+        x = self.l3(torch.concat([x , gamma], axis=-1))      #256+39-256
+        x = self.l2(x)      #256-256
+        x = self.l2(x)      #256-256
+        x = self.l4(x)      #256-128
+        x = self.l5(x)
         return output
--- Phase2/Wrapper.py
+++ Phase2/Wrapper.py
@@ -2,27 +2,51 @@ import argparse
 import glob
 from tqdm import tqdm
 import random
-from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
 import imageio
 import torch
 import matplotlib.pyplot as plt
 import os
+import json
+import cv2
 
 from NeRFModel import *
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 np.random.seed(0)
-
+print(device)
 def loadDataset(data_path, mode):
     """
     Input:
         data_path: dataset path
-        mode: train or test
+        mode: train or test or val
     Outputs:
         camera_info: image width, height, camera matrix 
         images: images
         pose: corresponding camera pose in world frame
     """
+    json_file_path = os.path.join(data_path, "transforms_" + mode)
+    json_file = glob.glob(json_file_path + ".json")
+    image_paths = []
+    with open(json_file[0]) as f:
+        data = json.load(f)
+        camera_fov = float(data['camera_angle_x'])
+        print(camera_fov)
+        pose = data['frames']
+        for i in range(len(pose)):
+            pose[i]['transform_matrix'] = torch.tensor(pose[i]['transform_matrix'])
+            pose[i]['rotation'] = torch.tensor(pose[i]['rotation'])
+            pose[i]['file_path'] = os.path.join(data_path, pose[i]['file_path'][2:])
+            image_paths.append(pose[i]['file_path'] + ".png")
+    images = []
+    for i in range(len(image_paths)):
+        img = cv2.imread(image_paths[i])
+        images.append(img)
+
+    camera_info = { "H" : images[0].shape[0], "W" : images[0].shape[1], \
+                   "f" : torch.tensor(0.5*images[0].shape[1]/np.tan(0.5*camera_fov)).to(device)}
+    return images, pose, camera_info
+
 
 def PixelToRay(camera_info, pose, pixelPosition, args):
     """
@@ -34,6 +58,16 @@ def PixelToRay(camera_info, pose, pixelPosition, args):
     Outputs:
         ray origin and direction
     """
+    width = camera_info['W']
+    height = camera_info['H']
+    focal_len = camera_info['f']
+    grid_x, grid_y = torch.meshgrid(torch.arange(width).to(device),torch.arange(height).to(device))
+    grid_x= grid_x.transpose(-1,-2)
+    grid_y = grid_y.transpose(-1,-2)
+    directions=torch.stack([(grid_x - width * .5) / focal_len, -(grid_y - height * .5) / focal_len, -torch.ones_like(grid_x)], dim=-1)
+    rays_dir=torch.sum(directions[..., None, :] *pose[:3, :3], dim=-1)
+    rays_ori= pose[:3,-1].expand(rays_dir.shape)
+    return rays_dir,rays_ori
 
 def generateBatch(images, poses, camera_info, args):
     """
@@ -56,20 +90,79 @@ def render(model, rays_origin, rays_direction, args):
         rgb values of input rays
     """
 
-def loss(groundtruth, prediction):
-
-
+def loss_fn(groundtruth, prediction):
+    """
+    Input:
+        groundtruth: groundtruth rgb values
+        prediction: predicted rgb values
+    Outputs:
+        loss
+    """
+    torch.nn.functional.mse_loss(rgb, img)
+    return loss
+def train_per_epoch(device, height, width, focal_len, cam_pose, near, far, sample, high_N, batch_size, model):
 def train(images, poses, camera_info, args):
-    
+    Parser = argparse.ArgumentParser()
+    Parser.add_argument('--nEpochs', type=int, default=1500)
+    Parser.add_argument('--samples', type=int, default=32)
+    Parser.add_argument('--MiniBatchSize', type=int, default=4096)
+    Parser.add_argument('--near', type=int, default=2)
+    Parser.add_argument('--far',type=int, default=6)
+    Parser.add_argument('--encode',type=int, default=6)
 
+    Args = Parser.parse_args()
+    num_epoch = Args.nEpochs
+    sample = Args.samples
+    batch_size = Args.MiniBatchSize
+    near = Args.near
+    far = Args.far
+    high_N = Args.dim_encode
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    focal_len = camera_info['f']
+    height, width = all_images.shape[1:3]
+
+    model = NeRF()
+    model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), 5e-3)
+
+    torch.manual_seed(9458)
+    random.seed(9458)
+    Loss = []
+    Epochs = []
+    for i in range(num_epoch+1):
+        img_idx = random.randint(0, images.shape[0]-1)
+        img = images[img_idx].to(device)
+        img=img.float()
+        cam_pose = poses[img_idx].to(device)
+        rgb,_,_ = train_per_epoch(device, height, width, focal_len, cam_pose, near, far, sample, high_N, batch_size, model)
+        loss= loss_fn(rgb, img)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        plt.imshow(rgb.detach().cpu().numpy())
+        plt.title(f"Iteration {i}")
+        plt.savefig("rgb.png")
+        Loss.append(loss.item())
+        Epochs.append(i+1)
+        plot_loss(Epochs, Loss)
+    SaveName =  './Checkpoint/' + 'model_' + str(i) + '.ckpt'
+    torch.save({'epoch': Epochs, 'model_state_dict': model.state_dict(), 'optimizer_state_dict': optimizer.state_dict()}, SaveName)
 def test(images, poses, camera_info, args):
-
+    """
+    Input:
+        images: all images in dataset
+        poses: corresponding camera pose in world frame
+        camera_info: image width, height, camera matrix
+        args: testing related parameters
+    Outputs:
+        rendered images
+    """
 
 def main(args):
+    
     # load data
     print("Loading data...")
     images, poses, camera_info = loadDataset(args.data_path, args.mode)
-
     if args.mode == 'train':
         print("Start training")
         train(images, poses, camera_info, args)
@@ -91,7 +184,6 @@ def configParser():
     parser.add_argument('--logs_path',default="./logs/",help="logs path")
     parser.add_argument('--checkpoint_path',default="./Phase2/example_checkpoint/",help="checkpoints path")
     parser.add_argument('--load_checkpoint',default=True,help="whether to load checkpoint or not")
-    parser.add_argument('--save_ckpt_iter',default=1000,help="num of iteration to save checkpoint")
     parser.add_argument('--images_path', default="./image/",help="folder to store images")
     return parser
 
